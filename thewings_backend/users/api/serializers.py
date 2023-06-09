bc95143efd1d8f3c292ebff5e924f81847e6bb22
tranvanan1005@gmail.com <tranvanan1005@gmail.com>
2023-06-09 16:07:00 +0700

    posts

--- thewings_backend/users/api/serializers.py
+++ thewings_backend/users/api/serializers.py
@@ -5,10 +5,19 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    avatar = serializers.SerializerMethodField()
+    f
     class Meta:
         model = User
-        fields = ["username", "name", "url"]
+        fields = ["username", "name", "url", "avatar"]
 
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "username"}
         }
+
+    def get_avatar(self, obj):
+        try:
+            return obj.profile.avatar.url if obj.profile.avatar else None
+        except:
+            return None
+            
