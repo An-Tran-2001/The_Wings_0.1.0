db10913f007d646ccab3c45e498b32738b3af093
tranvanan1005@gmail.com <tranvanan1005@gmail.com>
2023-06-14 09:05:40 +0700

    add peocess data

---
+++ Data_thewings/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Use processing for thewings app',
+    author='tranvanan1005@gmail.com',
+    license='MIT',
+)
