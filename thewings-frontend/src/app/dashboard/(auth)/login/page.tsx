8f3bd8db4f8fb93870b816cf94965361f96b96c6
tranvanan1005@gmail.com <tranvanan1005@gmail.com>
2023-06-15 08:55:12 +0700

    .

--- thewings-frontend/src/app/dashboard/(auth)/login/page.tsx
+++ thewings-frontend/src/app/dashboard/(auth)/login/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default () => {
     return(
         <div className = "w-full h-screen flex justify-center items-center">
-            <Image src={"/logo.png"} width={300} height={300} alt={"logo"} />
+            <p>login</p>
         </div>
     )
 }
